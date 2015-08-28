eaf75a1dea679cb826f8fe804f6d12613217c142
Victor Demin <demmbox@gmail.com>
2015-08-28 15:19:09 +0200

    max_allowed_packet. Added possibility to split query into multiple queries

--- CSVExporter.php
+++ CSVExporter.php
@@ -1,1 +1,30 @@
+<?php
 
+/**
+ * @copyright Copyright Victor Demin, 2015
+ * @license https://github.com/ruskid/yii2-excel-exporter/LICENSE
+ * @link https://github.com/ruskid/yii2-excel-exporter#README
+ */
+
+namespace ruskid\csvexporter;
+
+/**
+ * Little Yii2 Helper that exports Active Records to CSV
+ * @author Victor Demin <demin@trabeja.com>
+ */
+class CSVExporter extends \PHPExcel {
+
+    /**
+     * PHPExcel Settings here
+     */
+    public function __construct() {
+        parent::__construct();
+        $this->setActiveSheetIndex(0);//Use only one sheet
+    }
+    
+    
+    public function export(){
+        echo 1;
+    }
+    
+}
