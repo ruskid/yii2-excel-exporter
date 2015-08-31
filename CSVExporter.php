8f3a25f0b9abcc85b9cee877aa1568713d3d13d7
Victor Demin <demmbox@gmail.com>
2015-08-31 11:06:12 +0200

    max_allowed_packet. Added possibility to split query into multiple queries

--- CSVExporter.php
+++ CSVExporter.php
@@ -8,6 +8,8 @@
 
 namespace ruskid\csvexporter;
 
+use Exception;
+
 /**
  * Little Yii2 Helper that exports Active Records to CSV
  * @author Victor Demin <demin@trabeja.com>
@@ -15,16 +17,169 @@ namespace ruskid\csvexporter;
 class CSVExporter extends \PHPExcel {
 
     /**
-     * PHPExcel Settings here
+     * Active Records for export
+     * @var \yii\db\ActiveRecord 
+     */
+    public $models;
+
+    /**
+     *
+     * @var array
+     */
+    public $values;
+
+    /**
+     * All row style
+     * @var array 
+     */
+    public $style = [];
+
+    /**
+     * Headers row style
+     * @var array 
+     */
+    public $headerStyle = ['font' => ['bold' => true]];
+
+    /**
+     * Excel filename
+     * @var string 
+     */
+    public $filename = 'results';
+
+    /**
+     * Start point for export X|Y
+     * @var string 
+     */
+    public $startPoint = 'A1';
+    private $_letters = [];
+    private $_currentRowNumber;
+
+    /**
+     * PHPExcel Settings
      */
     public function __construct() {
         parent::__construct();
-        $this->setActiveSheetIndex(0);//Use only one sheet
+        $this->setActiveSheetIndex(0); //Use only one sheet
     }
-    
-    
-    public function export(){
-        echo 1;
+
+    /**
+     * Will validate that required parameters are present
+     * @throws Exception
+     */
+    private function validateInput() {
+        if (!$this->models) {
+            throw new Exception("'models' parameter is required in " . __CLASS__);
+        }
+        if (!$this->values) {
+            throw new Exception("'values' parameter is required in " . __CLASS__);
+        }
     }
-    
+
+    /**
+     * Will generate letters by calculating start point X axe and number of values
+     * @return array
+     */
+    public function getLetters() {
+        if (empty($this->_letters)) {
+            $x = $this->startPoint[0]; //use letter from start point
+            $count = 0;
+            $max = count($this->values);
+            for ($x; $x <= 'Z' && $count < $max; $x++) {
+                array_push($this->_letters, $x);
+                $count++;
+            }
+        }
+        return $this->_letters;
+    }
+
+    /**
+     * Will get current row number
+     * @return integer
+     */
+    public function getCurrentRowNumber() {
+        if ($this->_currentRowNumber === null) {//take from start point
+            $this->_currentRowNumber = $this->startPoint[1];
+        }
+        return $this->_currentRowNumber;
+    }
+
+    /**
+     * Set new current row number
+     * @param integer $new
+     */
+    public function setCurrentRowNumber($new) {
+        $this->_currentRowNumber = $new;
+    }
+
+    /**
+     * Will set headers of Excel file
+     */
+    private function setHeaders() {
+        $col = 0;
+        $letters = $this->getLetters();
+        $row = $this->getCurrentRowNumber();
+        foreach ($this->values as $config) {
+            $label = isset($config['header']) ? $config['header'] : "";
+            $this->getActiveSheet()
+                    ->setCellValue($letters[$col] . $row, $label)
+                    ->getStyle($letters[$col] . $row)
+                    ->applyFromArray($this->headerStyle);
+            $col++;
+        }
+        $this->setCurrentRowNumber( ++$row);
+    }
+
+    /**
+     * Will set Active Records values to excel
+     */
+    private function setRows() {
+        $letters = $this->getLetters();
+        $row = $this->getCurrentRowNumber();
+
+        if (!is_array($this->models)) {
+            $this->models = [$this->models];
+        }
+
+        foreach ($this->models as $model) {
+            $col = 0;
+            foreach ($this->values as $config) {
+                $value = call_user_func($config['value'], $model);
+                $cell = $this->getActiveSheet();
+                $cell->setCellValue($letters[$col] . $row, $value);
+                if (!empty($this->style)) {//styling takes time
+                    $cell->getStyle($letters[$col] . $row)->applyFromArray($this->style);
+                }
+                $col++;
+            }
+            $row++;
+        }
+        $this->setCurrentRowNumber($row);
+    }
+
+    /**
+     * Will export the results
+     * @return mixed
+     */
+    public function export() {
+        $this->validateInput();
+        $this->setHeaders();
+        $this->setRows();
+        return $this->sendFileToBrowser();
+    }
+
+    /**
+     * Will send file to the client's browser
+     */
+    private function sendFileToBrowser() {
+        // ** Redirect output to a client’s web browser (Excel2007)
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="' . $this->filename . '.xlsx"');
+        header("Pragma: "); //IE8 quick fix.
+        header("Cache-Control: "); //IE8 quick fix.
+
+        $objWriter = \PHPExcel_IOFactory::createWriter($this, 'Excel2007');
+        $objWriter->save('php://output');
+        \Yii::$app->end();
+    }
+
 }
--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# yii2-excel-exporter
+# Yii2 CSV Exporter
+This will export Active Records into Excel file
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist ruskid/yii2-excel-exporter "*"
+```
+
+or add
+
+```
+"ruskid/yii2-excel-exporter": "*"
+```
+
+to the require section of your `composer.json` file.
+
+Usage
+-----
+
+```php
+$exporter = new CSVExporter;
+$exporter->models = User::find()->all();
+$exporter->values = [
+    [
+        'header' => Yii::t('app', 'Email'),
+        'value' => function($object) {
+            return $object->email;
+        }
+    ],
+    [
+        'header' => Yii::t('app', 'Nombre'),
+        'value' => function($object) {
+            return $object->nombre;
+        }
+    ],
+    [
+        'header' => Yii::t('app', 'Parent'),
+        'value' => function($object) {
+            return $object->parent->nombre;
+        }
+    ],
+
+];
+
+//Optional
+$exporter->filename("reports-2015");
+$exporter->startPoint('B3');
+return $exporter->export();
+```
+- You can also add <b>styling</b>, see style and headerStyle parameters. 
